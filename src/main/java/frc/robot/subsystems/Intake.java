6531aa9a692f641560c5c5584959f5a2631ff932
Noah Halstead <nhalstead00@gmail.com>
2020-02-20 00:15:13 -0500

    Updated Intake Subsystem File for better Formatting and Added new methods

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,11 +1,16 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* Copyright (c) 2019 FIRST. All Rights Reserved.														 */
+/* Open Source Software - may be modified and shared by FRC teams. The code	 */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
+/* the project.																															 */
 /*----------------------------------------------------------------------------*/
 
 package frc.robot.subsystems;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
@@ -14,76 +19,155 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.Constants.IntakeConstants;
+import io.github.pseudoresonance.pixy2api.Pixy2;
+import io.github.pseudoresonance.pixy2api.Pixy2CCC.Block;
 
 public class Intake extends SubsystemBase {
-  
-  private Encoder encoder = new Encoder(Constants.EncoderArm.encoderPort1, Constants.EncoderArm.encoderPort2, false, Encoder.EncodingType.k2X);
-  private WPI_TalonSRX armMotor = new WPI_TalonSRX(Constants.EncoderArm.armMotor);
-  private WPI_TalonSRX intakeMotor = new WPI_TalonSRX(Constants.EncoderArm.intakeMotor);
-  private NetworkTableEntry ntEncoderValue = NetworkTableInstance.getDefault().getEntry("Ball Lift Encoder Value");
-  private NetworkTableEntry ntEncoderReset = NetworkTableInstance.getDefault().getEntry("Ball Lift Encoder Reset");
 
-  public Intake() {
+	private Encoder encoder = new Encoder(Constants.IntakeConstants.encoderPort1,
+			Constants.IntakeConstants.encoderPort2, false, Encoder.EncodingType.k2X);
+	private WPI_TalonSRX armMotor = new WPI_TalonSRX(Constants.IntakeConstants.armMotor);
+	private WPI_TalonSRX intakeMotor = new WPI_TalonSRX(Constants.IntakeConstants.intakeMotor);
+	private NetworkTableEntry ntEncoderValue = NetworkTableInstance.getDefault().getEntry("Ball Lift Encoder Value");
+	private NetworkTableEntry ntEncoderReset = NetworkTableInstance.getDefault().getEntry("Ball Lift Encoder Reset");
+	private NetworkTableEntry ntPixyStatus = NetworkTableInstance.getDefault().getEntry("Pixy Status");
+	private NetworkTableEntry ntPixyBlocks = NetworkTableInstance.getDefault().getEntry("Pixy Blocks");
+	private Pixy2 pixy = Pixy2.createInstance(IntakeConstants.pixyLinkType);;
 
-    /**
-     * Register the default value for the network tables
-     */
-    this.ntEncoderReset.setDefaultBoolean(false);
-    this.ntEncoderValue.setDouble(0.0);
+	public Intake() {
 
-    /**
-     * Setup the encoder setting
-     */
-    encoder.reset();
-    encoder.setDistancePerPulse(4./256.);
-    encoder.setMaxPeriod(.1);
-    encoder.setMinRate(10);
-    encoder.setReverseDirection(true);
-    encoder.setSamplesToAverage(5);
-  }
+		/**
+		 * Register the default value for the network tables
+		 */
+		this.ntEncoderReset.setDefaultBoolean(false);
+		this.ntEncoderValue.setDouble(0.0);
+		this.ntPixyStatus.setDefaultString("");
 
-  public double getEncoderValue(){
-    return encoder.getDistance();
-  }
+		/**
+		 * Setup the encoder setting
+		 */
+		encoder.reset();
+		encoder.setDistancePerPulse(4. / 256.);
+		encoder.setMaxPeriod(.1);
+		encoder.setMinRate(10);
+		encoder.setReverseDirection(true);
+		encoder.setSamplesToAverage(5);
 
-  // Get the offset of the low target and  current position
-  public double getEncoderFromLowValue(){
-    return this.getEncoderValue() - Constants.EncoderArm.lowTarget;
-  }
+		// Tries to Communicate with the Pixy at this moment
+		this.pixy.init(IntakeConstants.pixyLinkPort);
+	}
 
-  // Get the offset of the high target and  current position
-  public double getEncoderFromHighValue(){
-    return this.getEncoderValue() - Constants.EncoderArm.highTarget;
-  }
+	public double getEncoderValue() {
+		return encoder.getDistance();
+	}
 
-  public void encoderReset(){
-    encoder.reset();
-  }
+	// Get the offset of the low target and current position
+	public double getEncoderFromLowValue() {
+		return this.getEncoderValue() - Constants.IntakeConstants.lowTarget;
+	}
 
-  public void armRun(double power){
-    armMotor.set(power);
-  }
+	// Get the offset of the high target and current position
+	public double getEncoderFromHighValue() {
+		return this.getEncoderValue() - Constants.IntakeConstants.highTarget;
+	}
 
-  public void intakeMove(double power){
-    intakeMotor.set(power);
-  }
+	/**
+	 * Returns all of the Objects within the view of the Pixy2
+	 * 
+	 * @return All Blocks Found in the FOV
+	 */
+	public ArrayList<Block> getPixyBlocks() {
+		ArrayList<Block> blocks = this.pixy.getCCC().getBlocks();
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
+		ntPixyBlocks.setDouble(blocks.size());
 
-    /**
-     * Push Encoder Value to the Dashbaord via NetworkTables.
-     */
-    this.ntEncoderValue.setDouble(this.getEncoderValue());
+		return blocks;
+	}
 
-    /**
-     * Reset the Encoder from the Dashboard
-     */
-    if(this.ntEncoderReset.getBoolean(false) == true) {
-      this.encoderReset();
-      this.ntEncoderReset.setBoolean(false);
-    }
+	/**
+	 * This function will get the largest (inherently the closest) object
+	 *  from the list of the blocks in the detected FOV.
+	 * 
+	 * @return Block that is the closest
+	 */
+	public Block getPixyLargest() {
+		AtomicReference<Block> largestBlock = new AtomicReference<>();
+		AtomicInteger largestArea = new AtomicInteger(0);
 
-  }
+		/**
+		 * AtomicRefrnce and AtomicInteger is used to
+		 *  access and store variables between the different loops
+		 *  within the forEach Consumer Lambda Function.
+		 */
+		this.getPixyBlocks().forEach((block) -> {
+			int area = block.getWidth() * block.getHeight();
+
+			if(area > largestArea.get()) {
+				// Found a larger block!
+				largestBlock.set(block);
+				largestArea.set(area);
+			}
+		});
+
+		return largestBlock.get();
+	}
+
+
+	public void encoderReset() {
+		encoder.reset();
+	}
+
+	public void armRun(double power) {
+		armMotor.set(power);
+	}
+
+	public void intakeMove(double power) {
+		intakeMotor.set(power);
+	}
+
+	@Override
+	public void periodic() {
+		// This method will be called once per scheduler run
+
+		/**
+		 * Push Encoder Value to the Dashbaord via NetworkTables.
+		 */
+		this.ntEncoderValue.setDouble(this.getEncoderValue());
+
+		/**
+		 * Must be called in order for the subsystem to request
+		 *  blocks from the pixy.
+		 * Calling this function without the given params will
+		 *  return the previous resutls received in this request.
+		 * 
+		 */
+		int pixyStatus = this.pixy.getCCC().getBlocks(false, 0, 8);
+
+		/**
+		 * Read response from the Pixy Class to update the network tables
+		 *  of the current status.
+		 * 
+		 */
+		switch(pixyStatus) {
+			case Pixy2.PIXY_RESULT_BUSY:
+				this.ntPixyStatus.setString("Busy");
+				break;
+			case Pixy2.PIXY_RESULT_ERROR:
+				this.ntPixyStatus.setString("Error");
+				break;
+			case Pixy2.PIXY_RESULT_OK:
+			default:
+				this.ntPixyStatus.setString("Operational");
+		}
+
+		/**
+		 * Reset the Encoder from the Dashboard
+		 */
+		if (this.ntEncoderReset.getBoolean(false) == true) {
+			this.encoderReset();
+			this.ntEncoderReset.setBoolean(false);
+		}
+
+	}
 }
