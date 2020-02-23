e721f33f68b379076682cf7e7e865c73432e01d9
Asher Carney <ashman32@outlook.com>
2020-02-23 13:58:38 -0500

    Space to Tab Convert
    Co-Authored-By: Noah Halstead <nhalstead@users.noreply.github.com>

--- src/main/java/frc/robot/commands/liftControl.java
+++ src/main/java/frc/robot/commands/liftControl.java
@@ -13,50 +13,50 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.LiftSubsystem;
 
 public class liftControl extends CommandBase {
-  /**
-   * Creates a new liftControl.
-   */
-  private LiftSubsystem liftS;
-  private BooleanSupplier upButton;
-  private BooleanSupplier downButton;
+	/**
+	 * Creates a new liftControl.
+	 */
+	private LiftSubsystem liftS;
+	private BooleanSupplier upButton;
+	private BooleanSupplier downButton;
 
 
-  public liftControl(LiftSubsystem subsystem, BooleanSupplier UpButton, BooleanSupplier DownButton) {
-    this.liftS = subsystem;
-    this.upButton = UpButton;
-    this.downButton = DownButton;
-    addRequirements(subsystem);
-    // Use addRequirements() here to declare subsystem dependencies.
-  }
+	public liftControl(LiftSubsystem subsystem, BooleanSupplier UpButton, BooleanSupplier DownButton) {
+		this.liftS = subsystem;
+		this.upButton = UpButton;
+		this.downButton = DownButton;
+		addRequirements(subsystem);
+		// Use addRequirements() here to declare subsystem dependencies.
+	}
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-  }
+	// Called when the command is initially scheduled.
+	@Override
+	public void initialize() {
+	}
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    if(upButton.getAsBoolean()){
-      liftS.liftSpeed(0.95);
-    }
-    else if(downButton.getAsBoolean()){
-      liftS.liftSpeed(-0.95);
-    }
-    else{
-      liftS.liftSpeed(0);
-    }
-    
-  }
+	// Called every time the scheduler runs while the command is scheduled.
+	@Override
+	public void execute() {
+		if(upButton.getAsBoolean()){
+		liftS.liftSpeed(0.95);
+		}
+		else if(downButton.getAsBoolean()){
+		liftS.liftSpeed(-0.95);
+		}
+		else{
+		liftS.liftSpeed(0);
+		}
+		
+	}
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-  }
+	// Called once the command ends or is interrupted.
+	@Override
+	public void end(boolean interrupted) {
+	}
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
+	// Returns true when the command should end.
+	@Override
+	public boolean isFinished() {
+		return false;
+	}
 }
