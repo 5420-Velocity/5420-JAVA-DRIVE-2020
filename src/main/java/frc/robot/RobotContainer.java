f2a6159b94167ff0803c2c4e0a83ce34e72083b5
Noah Halstead <nhalstead00@gmail.com>
2021-01-24 22:57:17 -0500

    Adjusted the Motor Speed

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -193,7 +193,7 @@ public class RobotContainer {
 		 * Setup Button Events for the Shooter on the Operator Controller
 		 */
 		new JoystickButton(this.operatorJoystick, Constants.ControllerMapConstants.Blue_Button_ID)
-			.whenPressed(() -> this.newShooter.setSpeed(-0.7, 0))
+			.whenPressed(() -> this.newShooter.setSpeed(-0.64, 0))
 			.whenReleased(() -> this.newShooter.setSpeed(0, 0));
 
 		new JoystickButton(this.operatorJoystick, Constants.ControllerMapConstants.Left_Bumper)
