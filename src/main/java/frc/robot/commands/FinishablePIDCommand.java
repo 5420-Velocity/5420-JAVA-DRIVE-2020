831ecff173295e4ce03600e0f856c8bc6f1cb2f1
Noah Halstead <nhalstead00@gmail.com>
2021-03-06 17:17:39 -0500

    Update FinishablePIDCommand.java

--- src/main/java/frc/robot/commands/FinishablePIDCommand.java
+++ src/main/java/frc/robot/commands/FinishablePIDCommand.java
@@ -1,5 +1,6 @@
 package frc.robot.commands;
 
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.BooleanSupplier;
 import java.util.function.DoubleConsumer;
 import java.util.function.DoubleSupplier;
@@ -7,6 +8,7 @@ import java.util.function.DoubleSupplier;
 import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.PIDCommand;
 import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.utils.DoubleConsumerBooleanSupplier;
 
 import static edu.wpi.first.wpilibj.util.ErrorMessages.requireNonNullParam;
 
@@ -16,27 +18,36 @@ import static edu.wpi.first.wpilibj.util.ErrorMessages.requireNonNullParam;
  */
 public class FinishablePIDCommand extends PIDCommand {
 
-	private BooleanSupplier completeSupplier;
+	private DoubleConsumerBooleanSupplier completeSupplier;
+	private AtomicReference<Double> atomicOutput = new AtomicReference<Double>();
 
 	public FinishablePIDCommand(PIDController controller, DoubleSupplier measurementSource, double setpoint,
 			DoubleConsumer useOutput, Subsystem... requirements) {
 		super(controller, measurementSource, setpoint, useOutput, requirements);
 
-		this.completeSupplier = () -> false;
+		this.completeSupplier = (Double value) -> false;
 	}
 
 	public FinishablePIDCommand(PIDController controller, DoubleSupplier measurementSource, double setpoint,
-			DoubleConsumer useOutput, BooleanSupplier completeSupplier, Subsystem... requirements) {
+			DoubleConsumer useOutput, DoubleConsumerBooleanSupplier completeSupplier, Subsystem... requirements) {
 		super(controller, measurementSource, setpoint, useOutput, requirements);
 
 		requireNonNullParam(completeSupplier, "command", "FinishablePIDCommand");
 
 		this.completeSupplier = completeSupplier;
 	}
+
+	@Override
+	public void execute() {
+		this.atomicOutput.set(
+			m_controller.calculate(m_measurement.getAsDouble(), m_setpoint.getAsDouble())
+		);
+	  m_useOutput.accept(this.atomicOutput.get());
+	}
 	
 	@Override
 	public boolean isFinished() {
-		return this.completeSupplier.getAsBoolean();
+		return this.completeSupplier.getAsBoolean(this.atomicOutput.get());
 	}
 
 }
