9a089bdc7defe34c09a89f1984379b7a2f77bb00
Asher Carney <ashman32@outlook.com>
2021-03-20 18:22:53 -0400

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -3,6 +3,7 @@ package frc.robot;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.subsystems.DriveTrain;
@@ -28,6 +29,9 @@ public class Robot extends TimedRobot {
 		// autonomous chooser on the dashboard.
 		m_robotContainer = new RobotContainer();
 		m_robotContainer.limeLight.setLedMode(1);
+
+		
+		SmartDashboard.setDefaultString("Command", "idle");
 	}
 
 	/**
@@ -82,6 +86,8 @@ public class Robot extends TimedRobot {
 	@Override
 	public void teleopInit() {
 		m_robotContainer.intake.forceArmDown(false);
+
+		m_robotContainer.driveTrain.setBrakeMode(NeutralMode.Coast);
 
 		// This makes sure that the autonomous stops running when
 		// teleop starts running. If you want the autonomous to
