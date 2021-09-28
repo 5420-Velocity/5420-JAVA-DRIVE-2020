a7e67525b3ee22130445e937d9a20788f36dde20
Noah Halstead <nhalstead00@gmail.com>
2021-09-28 19:38:43 -0400

    Create RepeatedCommand.java

---
+++ src/main/java/frc/robot/commands/RepeatedCommand.java
@@ -0,0 +1,64 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/**
+ * RepeatedCommand
+ * This will run a set command a set amount of times then exit.
+ */
+public class RepeatedCommand extends CommandBase {
+
+	private final Command command;
+	private final int loopCountLimit;
+	private int loopCount;
+	private boolean isFinished = false;
+
+	public RepeatedCommand(Command command) {
+		this(2, command);
+	}
+
+	public RepeatedCommand(int loopCountLimit, Command command) {
+		this.command = command;
+		this.loopCountLimit = loopCountLimit;
+	}
+
+	@Override
+	public void initialize() {
+		this.isFinished = false;
+		this.loopCount = 0;
+	}
+
+	@Override
+	public void execute() {
+		if (this.loopCount == this.loopCountLimit) {
+			this.isFinished = true;
+			return;
+		}
+
+		if (!this.command.isScheduled()) {
+			// Not Scheduled
+			this.loopCount++;
+			this.command.schedule();
+		}
+	}
+
+	@Override
+	public void end(boolean interrupted) {
+		if (this.command.isScheduled()) {
+			this.command.cancel();
+		}
+		this.isFinished = true;
+	}
+
+	@Override
+	public boolean isFinished() {
+		// Hold the command open if the command is running.
+		return this.isFinished && this.command.isFinished();
+	}
+
+	@Override
+	public boolean runsWhenDisabled() {
+		return this.command.runsWhenDisabled();
+	}
+}
